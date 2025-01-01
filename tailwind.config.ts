3370a2179d877f47e54ac03c05c823f48871f6b3
jordonm08 <jordonm08@gmail.com>
2024-12-31 22:59:44 -0600

    Enhance Tailwind CSS configuration and update global styles
    - Added new color variables for card and muted states in Tailwind configuration to improve design consistency.
    - Updated global CSS variables to use hex color values for better clarity and maintainability.
    - Removed unused dark mode styles to streamline the CSS file.
    
    These changes enhance the styling setup and improve the overall visual presentation of the application.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,14 @@ export default {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
+        card: {
+          DEFAULT: 'var(--card)',
+          foreground: 'var(--card-foreground)',
+        },
+        muted: {
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
+        },
         primary: {
           DEFAULT: 'var(--primary)',
           foreground: 'var(--primary-foreground)',
